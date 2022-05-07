0f9fd257e4ddf6215cb7ef0f1615572d8682bf7c
Guigalaverna <guigalaverna@gmail.com>
2022-05-07 00:42:28 -0300

    feat: running script

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
+const yargs = require("yargs");
+const utils = require("./run");
 
-console.log("h");
+// show usage message
+const usage = "\nUsage: pjs <alias> <project name>";
+const options = yargs
+  .usage(usage)
+  .option("a", {
+    alias: "alias",
+    describe: "Alias of a setup configured.",
+    type: "string",
+    demandOption: false,
+  })
+  .option("p", {
+    alias: "projectName",
+    describe: "Name of the project to be created.",
+    type: "string",
+    demandOption: false,
+  })
+  .locale("en")
+  .help(true).argv;
+
+utils.run(yargs.argv._);
