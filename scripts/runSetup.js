0068ca43524efd5e5c821b40f4676ad5c0d94f34
Guigalaverna <guigalaverna@gmail.com>
2022-05-07 20:40:29 -0300

    feat: remove old folder

--- scripts/runSetup.js
+++
@@ -1,9 +0,0 @@
-const shell = require("shelljs");
-
-function execute(alias, projectName) {
-  shell.exec(
-    `PROJECT_NAME=${projectName} bash ~/.config/pjs/setups/${alias}.sh`
-  );
-}
-
-module.exports = { execute };
