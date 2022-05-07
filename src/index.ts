ed554f1b7cf7b3f8e755c148862c6dfd5950ccb7
Guigalaverna <guigalaverna@gmail.com>
2022-05-07 19:45:00 -0300

    feat: create Orchestrator

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { ScriptAdapter } from "../@types/adapters/script-adapter";
+import { SetupAdapter } from "../@types/adapters/setup-adapter";
+import yargs, { Arguments, Argv } from "yargs";
+
+import path from "path";
+import os from "os";
+import fs from "fs";
+
+export class Orchestrator {
+  constructor(
+    private setupAdapter: SetupAdapter,
+    private scriptAdapter: ScriptAdapter
+  ) {
+    this.setupAdapter = setupAdapter;
+    this.scriptAdapter = scriptAdapter;
+  }
+
+  async execute(args: yargs.Arguments) {
+    const { setupAdapter, scriptAdapter } = this;
+
+    const alias = (args.alias || args._[0]) as string;
+    const projectName = (args.projectName || args._[1]) as string;
+
+    const setups = setupAdapter.list();
+    const setup = setups.find(setup => setup.alias === alias);
+    const pathOfScript = path.join(
+      os.homedir(),
+      ".config",
+      "setups",
+      `${alias}.sh`
+    );
+    const setupScriptExists = fs.existsSync(pathOfScript);
+
+    if (!setup) {
+      throw new Error(`Setup with alias ${alias} not found.`);
+    }
+
+    if (!setupScriptExists) {
+      setupAdapter.create(setup);
+    }
+
+    scriptAdapter.execute(pathOfScript, projectName);
+  }
+
+  async listSetups() {}
+}
